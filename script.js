460cf55320ce1265e73739aa155191401d2b1c57
RJ Tony <ray.developper@gmail.com>
2021-10-26 23:41:22 +0100

    feat : Image

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const progressBar =  document.querySelector("#progress-nar");
 
 function moveProgress (){
   let scrollPosition = mainSection.getBoundingClientRect();
-  console.log(-scrollPosition.y);
+  console.log(scrollPosition);
+  
 }
 
 window.addEventListener("scroll",moveProgress)
