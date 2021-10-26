e3e1f991fa52a8de24f781c29f11adb0e2452c92
RJ Tony <ray.developper@gmail.com>
2021-10-26 23:50:54 +0100

    feat : Animate progress bar on Scroll

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const mainSection = document.querySelector("#section");
-const progressBar =  document.querySelector("#progress-nar");
+const progressBar =  document.querySelector("#progress-bar");
 
 function moveProgress (){
   let scrollPosition = mainSection.getBoundingClientRect();
-  console.log(scrollPosition);
-  
+  console.log(-scrollPosition.y);
+  let proWidth = (scrollPosition/(mainSection.getBoundingClientRect().height
+  -document.documentElement.clientHeight))*100;
+
+  progressBar.style.width = Math.floor(proWidth) + "%";
 }
 
 window.addEventListener("scroll",moveProgress)
