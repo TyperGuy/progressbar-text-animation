59a0760129c397718ec083bf59e6412e572468ea
RJ Tony <ray.developper@gmail.com>
2021-10-27 23:06:47 +0100

    refactor : Code udpdate

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const mainSection = document.querySelector("#section");
 const progressBar =  document.querySelector("#progress-bar");
 
+
+/* 
+ "Step By Step Function"
 function moveProgress (){
+
   let scrollPosition = mainSection.getBoundingClientRect().top;
+  
   let proWidth = (scrollPosition/(mainSection.getBoundingClientRect().height
   -document.documentElement.clientHeight))*(-100);
     console.log(Math.floor(proWidth));
 
-  // let  value = Math.floor(proWidth);
   progressBar.style.width = `${(Math.floor(proWidth))}%`;
   
 
 }
 
+*/
 
-window.addEventListener("scroll",moveProgress)
+
+
+window.addEventListener("scroll",()=>{
+  progressBar.style.width = `${(Math.floor((mainSection.getBoundingClientRect().top/(mainSection.getBoundingClientRect().height
+  -document.documentElement.clientHeight))*(-100)))}%`;
+})
