6dbbddc9234574dfb4683e31ce9bd7d717b7ca5a
RJ Tony <ray.developper@gmail.com>
2021-10-27 22:18:21 +0100

    fix : progress bar animation in width

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const progressBar =  document.querySelector("#progress-bar");
 
 function moveProgress (){
   let scrollPosition = mainSection.getBoundingClientRect().top;
-  console.log(scrollPosition.y);
   let proWidth = (scrollPosition/(mainSection.getBoundingClientRect().height
   -document.documentElement.clientHeight))*(-100);
+    console.log(Math.floor(proWidth));
 
   // let  value = Math.floor(proWidth);
+  progressBar.style.width = `${(Math.floor(proWidth))}%`;
+  
 
-
-  console.log()
 }
 
 
