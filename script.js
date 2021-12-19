93002cfeb5a3c8ae8d860982316e8938ee6a2676
TyperGuy <ray.developper@gmail.com>
2021-12-19 11:32:54 +0100

    feat : progress-bar percentage

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const mainSection = document.querySelector("#section");
 const progressBar =  document.querySelector("#progress-bar");
+const percent = document.querySelector("#percent");
 
 
 /* 
@@ -24,4 +25,5 @@ function moveProgress (){
 window.addEventListener("scroll",()=>{
   progressBar.style.width = `${(Math.floor((mainSection.getBoundingClientRect().top/(mainSection.getBoundingClientRect().height
   -document.documentElement.clientHeight))*(-100)))}%`;
+  percent.innerText(progressBar.style.width+"%");
 })
